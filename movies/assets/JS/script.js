75837626554502e1ad093cba838fe0de06a3f398
darrenthamtoro <darrenthamtoro@gmail.com>
2021-07-07 14:25:26 -0700

    added images to the buttons

---
+++ movies/assets/JS/script.js
@@ -0,0 +1,5 @@
+moviesBtnEl.addEventListener("click", function(){
+    window.location.href = 'movies.html'})
+
+trailsBtnEl.addEventListener("click", function(){
+    window.location.href = 'trails.html'})
