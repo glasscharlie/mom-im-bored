0e1de5afa59c3d13b7706a0aecd42aea6981f0ea
Charles Glass <charlie-glass@live.com>
2021-07-08 14:22:41 -0700

    fixed styling and refactored JS

--- trails/assets/JS/trails.js
+++ trails/assets/JS/trails.js
@@ -1,23 +1,28 @@
 var mainElement = document.querySelector("#content")
-var render = 0
+var cityName = document.querySelector("#city-input")
+var sumbitElement = document.querySelector("#submit-button")
+var locationArray = [];
+var trailLocations = []
 
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
-  } else {
-    console.log("Geolocation is not supported by this browser.")
-  }
-}
-getLocation()
-
-
-let locationArray = [];
-function successFunction(position) {
-  locationArray.push(new Location(position.coords.latitude,position.coords.longitude))
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
 
-function errorFunction(position) {
- console.warn('error')
+function getLocation(event) {
+  var citySearch = cityName.value
+  
+  var apiKey = 'a6f4c7c6117b467306e0dda6808a0ae4'
+  var requestUrl = `http://api.positionstack.com/v1/forward?access_key=${apiKey}&query=${citySearch}`;
+  
+  
+  fetch(requestUrl)
+  .then(function (response) {
+      return response.json();
+  })
+  .then(function (data) {
+      locationArray.push(new Location(data.data[0].latitude,data.data[0].longitude))
+      trailApi()
+  });
 }
 
 function Location(lat, lon) {
@@ -26,7 +31,6 @@ function Location(lat, lon) {
 }
 
 async function trailApi(event) {
-    await new Promise(r => setTimeout(r, 2000));
     fetch(`https://trailapi-trailapi.p.rapidapi.com/?q-activities_activity_type_name_eq=hiking&lon=${locationArray[0].lon}&limit=25&lat=${locationArray[0].lat}`, {
       "method": "GET",
       "headers": {
@@ -38,53 +42,65 @@ async function trailApi(event) {
       return response.json();
     })
     .then(response => {
-
+      trailLocations = []
       for (let i = 0; i < response.places.length; i++) {
-        if(response.places[i].description !== null && render < 4) {
-              
-            console.log(render)
-            var div = document.createElement("div")
-            div.setAttribute(`class`,`card`) 
-            div.className += ' cell small-4 large-offset-1'
-            div.style.width = '33%'
-            var titleDiv = document.createElement("div")
-            titleDiv.setAttribute(`class`,`card-divider`)
-            div.appendChild(titleDiv)
-            var title = document.createElement("h3")
-            var description = document.createElement("p")
-            title.textContent = response.places[i].name
-            description.textContent = response.places[i].description
-            mainElement.appendChild(div)
-            titleDiv.appendChild(title)
-            var mapDiv = document.createElement("div")
-            mapDiv.setAttribute(`id`,`map${i}`)
-            mapDiv.setAttribute(`class`,`mapDiv`)
-            mapDiv.style.height = "300px"
-            mapDiv.style.width = '400px'
-            div.appendChild(mapDiv)
-            var desDiv = document.createElement("div")
-            desDiv.setAttribute(`class`,`card-section`)
-            div.appendChild(desDiv)
-            desDiv.appendChild(description)
-            
-            var map = L.map(`map${i}`).setView([response.places[i].lat, response.places[i].lon], 13);
-
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(map);
-
-            L.marker([response.places[i].lat, response.places[i].lon]).addTo(map)
-            render++
-          }
-        
+        if(response.places[i].description !== null) {
+          trailLocations.push({name: response.places[i].name, description: response.places[i].description, lat: response.places[i].lat, lon: response.places[i].lon })
+        }
       }
-      })
+      render()
+    })
+    
+    
     .catch(err => {
       console.error(err);
     });
-
-
+    
+    
   }
   
-    trailApi()
+  
+  sumbitElement.addEventListener("click", getLocation)
+  
+  function render() {
+  console.log(trailLocations)
+  mainElement.innerHTML = ''
+  var randomNumber = getRandomInt(trailLocations.length)
+  var div = document.createElement("div")
+  div.setAttribute(`class`,`card`) 
+  div.className += ' card cell small-4 small-offset-4 large-4 large-offset-4'
+  div.style.width = '33%'
+  var titleDiv = document.createElement("div")
+  titleDiv.setAttribute(`class`,`card-divider`)
+  div.appendChild(titleDiv)
+  var title = document.createElement("h3")
+  title.setAttribute(`class`,`cell`)
+  var button = document.createElement("button")
+  button.textContent = 'Find another park'
+  div.appendChild(button)
+  var description = document.createElement("p")
+  title.textContent = trailLocations[randomNumber].name
+  description.textContent = trailLocations[randomNumber].description
+  mainElement.appendChild(div)
+  titleDiv.appendChild(title)
+  var mapDiv = document.createElement("div")
+  mapDiv.setAttribute(`id`,`map`)
+  mapDiv.setAttribute(`class`,`mapDiv`)
+  mapDiv.style.height = "300px"
+  mapDiv.style.width = '600px'
+  div.appendChild(mapDiv)
+  var desDiv = document.createElement("div")
+  desDiv.setAttribute(`class`,`card-section`)
+  div.appendChild(desDiv)
+  desDiv.appendChild(description)
+  
+  var map = L.map(`map`).setView([trailLocations[randomNumber].lat, trailLocations[randomNumber].lon], 13);
 
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  L.marker([trailLocations[randomNumber].lat, trailLocations[randomNumber].lon]).addTo(map)
+
+  button.addEventListener("click", render)
+}
